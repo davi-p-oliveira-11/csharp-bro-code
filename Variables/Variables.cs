086a10cc125a5b70cd1bc8597fada4961b1899c1
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-23 21:34:34 -0300

    added variables folder

--- Variables/Variables.cs
+++ Variables/Variables.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace bro-code-c#.Variables
+namespace Variables
 {
     public class Variables
     {
-        
+        static void Main(string[] args)
+        {
+            int x; // declaration
+            x = 123; // initialization
+
+            int y = 321; // declaration + initialization
+
+            int z = x + y;
+
+            int age = 21.5; // whole integer
+            double height = 300.5; // decimal
+            bool alive = false; // boolean
+            char symbol = '@'; // character
+            string name "John" // String
+
+            
+            Console.WriteLine("Your age is " + age );
+            Console.WriteLine("Your height is " + height + " cm" );
+            Console.WriteLine("Are you alive" + alive );
+            Console.WriteLine("Your symbol is" + symbol );
+
+            String userName = symbol + name;
+
+            Console.WriteLine("Your username is: " + userName)
+
+            Console.ReadKey();
+        }
     }
 }
