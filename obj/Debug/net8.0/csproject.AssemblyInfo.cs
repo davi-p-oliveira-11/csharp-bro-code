6b86ef218b479d55859b5d7c2bd6f56cb80ed2d1
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-28 17:29:35 -0300

    added number guessing game

--- obj/Debug/net8.0/csproject.AssemblyInfo.cs
+++ obj/Debug/net8.0/csproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb53662a78fa36a43b1e9c00802422b3727b7bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee7916a3b0f3ce8e09971bfb87217cb8e95673c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("csproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
