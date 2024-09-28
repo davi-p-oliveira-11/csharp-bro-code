2af75d9798c03fa85e1f8d83264c60e8b37c8227
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-28 03:09:15 -0300

    added hypotenuse calc

---
+++ HypotenuseCalc/HypotenuseCalc.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HypotenuseCalc
+{
+    public class HypotenuseCalc
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The hypotenuse is: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
