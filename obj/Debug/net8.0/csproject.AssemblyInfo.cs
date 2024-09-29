38432e689a75c5dba85ef4fe049efc8816eb7075
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-28 22:14:08 -0300

    Calculator added

--- obj/Debug/net8.0/csproject.AssemblyInfo.cs
+++ obj/Debug/net8.0/csproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd6898010b04b3d8f096c75d0cc0fe721cd8b31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5c396e592a0485e948e524b7414faef1971209")]
 [assembly: System.Reflection.AssemblyProductAttribute("csproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
